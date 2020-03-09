8fecbfdef3e5827b4b5a640126d11295eceb47e9
kelving <kelvin.vitone@gmail.com>
2020-03-09 23:15:01 +0300

    ui

--- lib/UI/pages/Dashboard/fiis/fees_Page.dart
+++ lib/UI/pages/Dashboard/fiis/fees_Page.dart
@@ -35,33 +35,54 @@ class _WallPageState extends State<FeesPage>
   String buttonLabel = 'Global';
   Fees fees;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  // }
+  @override
+  void initState() {
+    // controller = ScrollController()..addListener(_scrollListener);
+    super.initState();
+  }
 
-  // @override
-  // void dispose() {
-  //   super.dispose();
+  @override
+  void dispose() {
+    // controller.removeListener(_scrollListener);
+    super.dispose();
+  }
+
+  // void _scrollListener() {
+  //   if (model.state == ViewState.Idle) {
+  //     if (controller.position.pixels == controller.position.maxScrollExtent) {
+  //       // setState(() => _isLoading = true);
+  //       model.getWall(stdDiv_Global);
+  //       // scaffoldKey.currentState.widget
+  //     }
+  //   }
   // }
 
   @override
   Widget build(BuildContext context) {
     var userType = Provider.of<UserType>(context, listen: false);
     targeteid = widget.user.id;
+    User currentUser = Provider.of<User>(context, listen: false);
     if (userType == UserType.TEACHER) {
       isTeacher = true;
     } else if (userType == UserType.PARENT) {}
-    if (userType == UserType.STUDENT)
-      return BaseView<FeesPageModel>(
-        onModelReady: (model) => model.getFees(targeteid),
-        builder: (context, model, child) {
-          this.model = model;
-          return FeesViewer(
-            fees: Fees.fromSnapshot(model.feessnapshot),
-          );
-        },
-      );
+    if (userType == UserType.STUDENT) {
+      if (!isLoaded) {
+        // stdDiv_Global =
+        //     currentUser.standard + currentUser.division.toUpperCase();
+        isLoaded = true;
+      }
+
+      print(currentUser.id);
+    }
+    return BaseView<FeesPageModel>(
+      onModelReady: (model) => model.getFees(targeteid),
+      builder: (context, model, child) {
+        this.model = model;
+        return FeesViewer(
+          fees: Fees.fromSnapshot(model.feessnapshot),
+        );
+      },
+    );
   }
 
   @override
