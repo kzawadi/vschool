0929d961765d466da48976f843e6053a04f1dbb1
kelving <kelvin.vitone@gmail.com>
2020-04-24 01:30:14 +0300

    new repo

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\kelvi\AndroidStudioProjects\Academic-Connect"
+export "FLUTTER_APPLICATION_PATH=C:\Users\kelvi\Desktop\VSCHOOL ASSETS\shule\vschool"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/main.dart
+++ lib/main.dart
@@ -115,3 +115,5 @@ class OurSchoolApp extends StatelessWidget with Services {
     }
   }
 }
+
+//! a new repo
