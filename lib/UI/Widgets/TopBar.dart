228aca77711b5a2e04a47dc9c026074ff44fde6c
kelvin <kelvin.vitone@gmail.com>
2020-06-29 04:11:38 +0300

    setting prefered size and hacking a way to solve rapidly hero tags

--- lib/UI/Widgets/TopBar.dart
+++ lib/UI/Widgets/TopBar.dart
@@ -17,7 +17,7 @@ class TopBar extends StatefulWidget implements PreferredSizeWidget {
       @required this.onPressed,
       this.buttonHeroTag = 'topBarBtn',
       this.onTitleTapped})
-      : preferredSize = Size.fromHeight(60.0);
+      : preferredSize = Size.fromHeight(48.0);
 
   @override
   _TopBarState createState() => _TopBarState();
@@ -56,6 +56,104 @@ class _TopBarState extends State<TopBar> {
               // ),
               Hero(
                 tag: 'title',
+                transitionOnUserGestures: true,
+                child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: widget.onTitleTapped,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      height: 40,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Text(
+                              widget.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                // color: Colors.black54,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+///this is created to eliminat the heri tag issue ''title
+class TopBarAlternative extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+  final Widget child;
+  final Function onPressed;
+  final Function onTitleTapped;
+  final String buttonHeroTag;
+
+  @override
+  final Size preferredSize;
+
+  TopBarAlternative(
+      {@required this.title,
+      @required this.child,
+      @required this.onPressed,
+      this.buttonHeroTag = 'topBarBtn',
+      this.onTitleTapped})
+      : preferredSize = Size.fromHeight(48.0);
+
+  @override
+  _TopBarAlternativeState createState() => _TopBarAlternativeState();
+}
+
+class _TopBarAlternativeState extends State<TopBarAlternative> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          // SizedBox(height: 30,),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Hero(
+                transitionOnUserGestures: true,
+                tag: widget.buttonHeroTag,
+                child: Container(
+                  // elevation: 10,
+                  // shape: kBackButtonShape,
+                  child: MaterialButton(
+                    height: 40,
+                    minWidth: 50,
+                    elevation: 10,
+                    shape: kBackButtonShape,
+                    onPressed: widget.onPressed,
+                    child: widget.child,
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   width: 50,
+              // ),
+              Hero(
+                tag: 'titleAlternative',
                 transitionOnUserGestures: true,
                 child: Card(
                   elevation: 10,
