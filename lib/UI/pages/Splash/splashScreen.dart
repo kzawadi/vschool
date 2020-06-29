c8aec9c885b4f1edbda8f3dd4a2144f6dc9e9fa8
kelvin <kelvin.vitone@gmail.com>
2020-06-29 04:32:53 +0300

    splash screen back ground changes

--- lib/UI/pages/Splash/splashScreen.dart
+++ lib/UI/pages/Splash/splashScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ourESchool/UI/Utility/ui_helpers.dart';
+import 'package:ourESchool/UI/resources/colors.dart';
 import 'package:ourESchool/main.dart';
 
 class Splashscreen extends StatefulWidget {
@@ -23,8 +24,8 @@ class _SplashState extends State<Splashscreen> {
         //Future.delayed(Duration(microseconds: 500)),
         startAnimation: 'Alarm',
         // todo theme is not reloading so this below code is kinder lame will work on it
-        backgroundColor:
-            isThemeCurrentlyDark(context) ? Colors.pink[50] : Colors.black,
+        backgroundColor: MyColors.lightMilky,
+        // isThemeCurrentlyDark(context) ? Colors.pink[50] : Colors.black,
         //Colors.pink[100] : Colors.black,
         next: (_) => MyApp(),
       ),
