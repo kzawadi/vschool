7e0b7c357ef9ca0f77eb6838cf39fbf3d0396869
kelvin <kelvin.vitone@gmail.com>
2020-07-24 13:18:12 +0300

    system overlay in startup animation and future.delayed of 2 seconds

--- lib/UI/pages/Splash/splashScreen.dart
+++ lib/UI/pages/Splash/splashScreen.dart
@@ -2,6 +2,7 @@ import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ourESchool/UI/Utility/ui_helpers.dart';
 import 'package:ourESchool/UI/resources/colors.dart';
+import 'package:ourESchool/imports.dart';
 import 'package:ourESchool/main.dart';
 
 class Splashscreen extends StatefulWidget {
@@ -17,23 +18,34 @@ class _SplashState extends State<Splashscreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen.navigate(
-        alignment: Alignment.center,
-        name: 'assets/flare_anim/Loading.flr',
-        until: () => _appEngineStarted(),
-        loopAnimation: 'Loading.flr',
-        //Future.delayed(Duration(microseconds: 500)),
-        startAnimation: 'Alarm',
-        // todo theme is not reloading so this below code is kinder lame will work on it
-        backgroundColor: MyColors.lightMilky,
-        // isThemeCurrentlyDark(context) ? Colors.pink[50] : Colors.black,
-        //Colors.pink[100] : Colors.black,
-        next: (_) => MyApp(),
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent, // transparent status bar
+          systemNavigationBarColor: Colors.transparent, // navigation bar color
+          statusBarIconBrightness: isThemeCurrentlyDark(context)
+              ? Brightness.light //todo fixed theme preferences not reloading
+              : Brightness.dark, // status bar icons' color
+          systemNavigationBarIconBrightness:
+              Brightness.dark, //navigation bar icons' color
+        ),
+        child: SplashScreen.navigate(
+          alignment: Alignment.center,
+          name: 'assets/flare_anim/Loading.flr',
+          until: () => Future.delayed(Duration(seconds: 2)),
+          loopAnimation: 'Loading.flr',
+          //Future.delayed(Duration(microseconds: 500)),
+          startAnimation: 'Alarm',
+          // todo theme is not reloading so this below code is kinder lame will work on it
+          backgroundColor: MyColors.lightMilky,
+          // isThemeCurrentlyDark(context) ? Colors.pink[50] : Colors.black,
+          //Colors.pink[100] : Colors.black,
+          next: (_) => MyApp(),
+        ),
       ),
     );
   }
 
-  _appEngineStarted() {
-    WidgetsFlutterBinding.ensureInitialized();
-  }
+  // _appEngineStarted() {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  // }
 }
