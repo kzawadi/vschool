38fe33b08df2188ac182f06d4978f8d20a42b66f
kelvin <kelvin.vitone@gmail.com>
2020-08-07 14:53:48 +0300

    removing unneceesary imports

--- lib/core/services/feed_services/feed_services.dart
+++ lib/core/services/feed_services/feed_services.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ourESchool/UI/resources/utility.dart';
-import 'package:ourESchool/core/Models/feed/feed.dart';
 import 'package:ourESchool/core/services/Services.dart';
 import 'package:ourESchool/imports.dart';
 import 'package:rxdart/rxdart.dart';
