f838a4bcf6a36056dc2e80759ca7a5d75fceffd7
kelvin <kelvin.vitone@gmail.com>
2020-10-09 23:32:55 +0300

    Library page now get data from the cloud and some basic UI but state management need improvement becouse the list doesnt show up until you come back again

--- lib/UI/pages/libary/library_widgets/library_page.dart
+++ lib/UI/pages/libary/library_widgets/library_page.dart
@@ -4,6 +4,7 @@ import 'package:ourESchool/UI/pages/libary/library_ViewModel.dart';
 import 'package:ourESchool/UI/resources/customWidgets.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:ourESchool/core/Models/library/library_model.dart';
+import 'package:ourESchool/UI/Widgets/TopBar.dart';
 
 class LibraryPage extends StatelessWidget {
   @override
@@ -13,6 +14,12 @@ class LibraryPage extends StatelessWidget {
       builder: (context, model, child) {
         print('PartialBuildsView rebuilt');
         return Scaffold(
+          appBar: TopBar(
+            buttonHeroTag: 'Library',
+            title: "Library",
+            onPressed: null,
+            child: null,
+          ),
           body: Center(
             child: model.isBusy
                 ? Container(child: Text('Fetching Library Data'))
@@ -24,41 +31,7 @@ class LibraryPage extends StatelessWidget {
                         itemCount: model.library.length,
                         itemBuilder: (context, index) {
                           Classlibrary library = model.library[index];
-                          return SingleChildScrollView(
-                            child: Card(
-                              child: ExpansionTile(
-                                title: Text(
-                                  library.day,
-                                ),
-                                subtitle: Text(library.description),
-                                children: <Widget>[
-                                  ListView.builder(
-                                    itemCount: library.subjects.length,
-                                    itemBuilder: (context, index) {
-                                      var s = library.subjects[index];
-                                      return Card(
-                                        child: Text(s),
-                                      );
-                                    },
-                                  ),
-                                  // ListTile(
-                                  //   title: Text(
-                                  //     library.description,
-                                  //     textAlign: TextAlign.center,
-                                  //   ),
-                                  // subtitle: ListView.builder(
-                                  //   itemCount: library.subjects.length,
-                                  //   itemBuilder: (context, index) {
-                                  //     var s = library.subjects[index];
-                                  //     return Card(
-                                  //       child: Text(s),
-                                  //     );
-                                  //   },
-                                  // ),
-                                ],
-                              ),
-                            ),
-                          );
+                          return EntryItem(entry: library);
                         },
                       ),
           ),
@@ -66,5 +39,79 @@ class LibraryPage extends StatelessWidget {
       },
       viewModelBuilder: () => LibaryViewModel(),
     );
+  }
+}
+
+class EntryItem extends StatelessWidget {
+  const EntryItem({this.entry});
+  final Classlibrary entry;
+
+  // This function recursively creates the multi-level list rows.
+  Widget _buildTiles(BuildContext context, Classlibrary root) {
+    if (root.subjects.isEmpty) {
+      return ListTile(
+        title: Text(root.by),
+      );
+    }
+    return ExpansionTile(
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+            border: new Border(
+              right: new BorderSide(width: 1.0, color: Colors.white24),
+            ),
+          ),
+          child: Icon(Icons.ac_unit, color: Colors.black),
+        ),
+        key: PageStorageKey<Classlibrary>(root),
+        title: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              height: 65,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 0,
+                child: Center(
+                  child: Text(
+                    root.day,
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(fontSize: 17),
+                  ),
+                ),
+              ),
+            ),
+            // Text(
+            //   root.day,
+            //   style: Theme.of(context).textTheme.button.copyWith(fontSize: 17),
+            // ),
+            SizedBox(height: 20),
+            Text(root.description),
+            SizedBox(height: 30),
+          ],
+        ),
+        children: root.subjects.map((e) {
+          var w = Padding(
+            padding: EdgeInsets.only(left: 10.0, top: 10),
+            child: Column(
+              children: [
+                // Text(root.description),
+                // SizedBox(
+                //   height: 20,
+                // ),
+                Text(e),
+              ],
+            ),
+          );
+
+          return w;
+        }).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(context, entry);
   }
 }
